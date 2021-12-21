ad300cf8722d5184ab1e5238ee51db8345a88c0f
Ujjwal <meujjwal@gmail.com>
2021-12-21 15:17:46 +0545

    emoji  in message

--- app/models/emoji.js
+++ app/models/emoji.js
@@ -30,6 +30,42 @@ var mysql       = require('mysql');
     });
 }
 
+/**
+ * 
+ * @param {*} connection 
+ * @param {*} alias 
+ * @returns 
+ */
+ async function getEmojiByCode(connection, channelName, alias) {
+    return new Promise(function (resolve, reject) {
+        try {
+            var sql = mysql.format('select e.* from emojis e inner join channels c on c.id = e.channel_id where c.name = ? and e.alias = ? and e.deleted_at is null', [channelName, alias]);
+            connection.getConnection((err, conn) => {
+                if(err) {
+                    console.log(err);
+                }
+
+                conn.query(sql, function (error, results, fields) {
+                    if (error) {
+                        console.log(error);
+                    }
+
+                    conn.release();
+
+                    if(typeof results !== "undefined" && results.length > 0) {
+                        resolve(results[0]);
+                    } else {
+                        resolve();
+                    }
+                });
+            });
+        } catch(e) {
+            reject();
+        }
+    });
+}
+
 module.exports = {
-    get
+    get,
+    getEmojiByCode
 }
