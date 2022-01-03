00f218592f3fd30a4854756057d8c1647b892fed
Ujjwal <meujjwal@gmail.com>
2022-01-03 16:58:07 +0545

    system type fix

--- app/config/message.js
+++ app/config/message.js
@@ -1,8 +1,6 @@
 module.exports = {
     type: {
-        system: {
-
-        },
+        system          : "system",
         messaging: {
             send:           "dm",
             history:        "dch",
@@ -18,7 +16,7 @@ module.exports = {
         },
         user: {
             ban: "ub"
-        }
+        },
     },
     response: {
         messaging: {
